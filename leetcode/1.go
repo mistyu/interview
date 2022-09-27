576bf31eb59a4469a1393048e659e7d7ec629e99
li chenhui <821491377@qq.com>
2022-09-27 21:42:43 +0800

    feat: leetcode 1

--- leetcode/1.go
+++ leetcode/1.go
@@ -1,11 +1,11 @@
 func twoSum(nums []int, target int) []int {
-	hashMap := make(map[int] int)
 	// 创建一个空 hash 表
+	hashMap := make(map[int]int)
 	for i, num := range nums {
-			if j, ok := hashMap[target - num]; ok {
-					return []int{i, j}
-			}
-			hashMap[num] = i
+		if j, ok := hashMap[target-num]; ok {
+			return []int{i, j}
+		}
+		hashMap[num] = i
 	}
 	return nil
 }

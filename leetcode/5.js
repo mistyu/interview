9dc941993d4e56e677da4e188f3ebee910e63ff1
li chenhui <lichenhui821@qq.com>
2022-10-06 21:04:30 +0800

    feat: leetcode 5

---
+++ leetcode/5.js
@@ -0,0 +1,28 @@
+const longestPalindrome = s => {
+  if (s.length < 2) return s
+   let max = 1
+   let res = s[0]
+   for (let i = 0; i < s.length; i++) {
+      let left = i
+      let right = i + 1
+      while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > max) {
+        res = s.slice(left, right + 1)
+        max = right - left + 1 
+      }
+      left--
+      right++
+      }
+      left = i
+      right = i + 1
+      while (left >= 0 && right < s.length && s[left - 1] === s[right]) {
+      if (right - (left - 1) + 1 > max) {
+          res = s.slice(left - 1, right + 1)
+          max = right - (left - 1) + 1 
+      }
+          left--
+          right++
+       }
+   }
+   return res
+};

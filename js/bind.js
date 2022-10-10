55a8cfa9db53d0ba4f6a4c9a4cefec61a3bdcfe0
li chenhui <lichenhui821@qq.com>
2022-10-10 20:51:31 +0800

    fix: js bind

--- js/bind.js
+++ js/bind.js
@@ -1,11 +1,13 @@
-Function.prototype.bind = (_this, params) => {
+// 返回一个函数
+Function.prototype.bind = (_this, ...params) => {
   _this = _this == null ? window : _this
   if (!/^(function|object)$/.test(_this)) {
     _this = Object(_this)
   }
   const key = new Symbol('bindThis')
   _this[key] = this
-  const result = _this[key](...params)
-  delete _this[key]
-  return result
+  return async (...args) => {
+    await _this[key](...params, ...args)
+    delete _this[key]
+  }
 }

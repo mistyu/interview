ffc46fce711887dcf740f226f61a953a1cf8e69b
li chenhui <lichenhui821@qq.com>
2022-10-31 10:11:46 +0800

    perf: utils sleep用Promise实现

--- utils/sleep.js
+++ utils/sleep.js
@@ -1,11 +1,19 @@
-const getTime = () => (new Date()).getTime()
+// const getTime = () => (new Date()).getTime()
+
+// const sleep = delay => {
+//   const startTime = getTime()
+
+//   while (getTime() - startTime < delay) {
+//     continue
+//   }
+
+//   return true
+// }
 
 const sleep = delay => {
-  const startTime = getTime()
-
-  while (getTime() - startTime < delay) {
-    continue
-  }
-
-  return true
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, delay)
+  })
 }

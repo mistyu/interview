c8f5abfc793b2851551b743e71faf11293417f60
li chenhui <lichenhui821@qq.com>
2022-11-08 11:08:00 +0800

    feat: leetcode 剑指offer46

--- leetcode/剑指offer/剑指offer46.js
+++ leetcode/剑指offer/剑指offer46.js
@@ -1,8 +1,27 @@
-const minNumber = nums => {
-  return nums.sort((a, b) => {
-    const stra = a + ''
-    const strb = b + ''
-    // 大于 0 的话 b 会在 a 前面；小于 0 的话 a 会在 b 前面
-    return (stra + strb) - (strb + stra)
-  }).join('')
+var translateNum = function(num) {
+  if (num < 10) return 1
+
+  const str = String(num)
+  const dp = []
+  // 初始化 dp
+  dp[0] = 1
+  const isOnlyOne = (str1, str2) => {
+    if (Number(str1) > 2 || Number(str1) === 2 && Number(str2) > 5 || Number(str1) === 0) return true
+    return false
+  }
+  if (isOnlyOne(str[0], str[1])) {
+    dp[1] = 1
+  } else {
+    dp[1] = 2
+  }
+
+  for (let i = 2; i < str.length; i++) {
+    if (isOnlyOne(str[i-1], str[i])) {
+      dp[i] = dp[i - 1]
+    } else {
+      dp[i] = dp[i - 1] + dp[i - 2]
+    }
+  }
+  
+  return dp[str.length - 1]
 }

6685b5ec79486adf0be81f2977d54965bacdebeb
li chenhui <lichenhui821@qq.com>
2022-11-11 21:12:17 +0800

    feat: leetcode 剑指offer61

---
+++ data-structures-algorithms/leetcode/剑指offer/剑指offer61.js
@@ -0,0 +1,19 @@
+function isStraight (nums) {
+	const set = new Set()
+	let min = 14
+	let max = 0
+
+	for (num of nums) {
+		if (num === 0) continue
+		max = Math.max(num, max)
+		min = Math.min(num, min)
+
+		if (set.has(num)) {
+			return false
+		}
+
+		set.add(num)
+	}
+
+	return max - min < 5
+}

b28c7b061b14cc03d644d8e15c4b09c0573fa717
li chenhui <lichenhui821@qq.com>
2022-11-29 16:34:44 +0800

    fix: utils flattenFns 修复异步函数问题

--- utils/flattenPromise.js
+++ utils/flattenPromise.js
@@ -2,5 +2,5 @@
 function flattenFns (fns) {
   fns = Array.isArray(fns) ? fns : [fns]
   // 将 fns 链式调用 -> Promise.resolve().then(() => fn1(...args)).then(() => fn2(...args)).then(() => fn3(...args)) ...
-  return (...args) => fns.reduce((p, fn) => p.then(() => fn(...args)), Promise.resolve())
+  return (...args) => fns.reduce((p, fn) => p.then(async () => await fn(...args)), Promise.resolve())
 }

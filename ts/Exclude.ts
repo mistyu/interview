d8636e788cb41b43fe381f186f6fa2f22472e658
li chenhui <lichenhui821@qq.com>
2022-12-09 18:45:36 +0800

    feat: ts Exclude

--- ts/Exclude.ts
+++ ts/Exclude.ts
@@ -1,5 +1,5 @@
 // 把 T, U 中共有的属性去掉
-type Exclude<T, U> = T extends U ? never : T
+export type Exclude<T, U> = T extends U ? never : T
 
 type A = 'a' | 'b' | 'c'
 type B = 'c' | 'd'
@@ -7,3 +7,4 @@ type B = 'c' | 'd'
 type C = Exclude<A, B>
 
 export {}
+// 接收两个联合类型 T U 将 T 中 U 的属性去掉
